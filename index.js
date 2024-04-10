9a49c7e253f4d247af09cccb33be102e6c610000
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-10 18:41:15 -0500

    connected to DB

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+// import dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// create an express app
+const app = express();
+
+// load environmental variables
+dotenv.config();
+
+// declare port
+const PORT = process.env.PORT || 8080;
+
+// get the MongoDB connect URL from the environment variable
+const MONGOURL = process.env.MONGO_URL;
+
+// connect to MongoDB using Mongoose
+mongoose.connect(MONGOURL)
+    .then(() => {
+        console.log('Connected to MongoDB')
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    })
